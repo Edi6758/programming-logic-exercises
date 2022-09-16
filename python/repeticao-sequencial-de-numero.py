64dd2100604f3ea3e76adc12dfc284a7d095a2da
Edi6758 <ediarts7@gmail.com>
2022-09-15 23:20:04 -0300

    :tada: first commit and resolution of an array exercise

---
+++ python/repeticao-sequencial-de-numero.py
@@ -0,0 +1,11 @@
+#Escreva um programa que mostre na tela a seguinte contagem:
+#6 7 8 9 10 11 Acabou
+def repetirNumero():
+    valor = int(input('Digite o valor 6 por favor '))
+    lista = []
+    for valor in range(valor ,valor+7):
+        lista.append(valor)
+    textoLIsta = " ".join(map(str,lista))
+    print(f'{textoLIsta} Acabou')
+
+repetirNumero()
